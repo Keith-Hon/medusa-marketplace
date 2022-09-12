533a0d148a9f394e44f155e2e07e9cedf4526547
Keith Hon <keith.wkhon@gmail.com>
2022-09-12 15:20:20 +0800

    converted .js to .ts in medusa-core-utils package

---
+++ packages/medusa-core-utils/src/zero-decimal-currencies.ts
@@ -0,0 +1,3 @@
+const zeroDecimalCurrencies = ["bif", "clp", "djf", "gnf", "jpy", "kmf", "krw", "mga", "pyg", "rwf", "ugx", "vnd", "vuv", "xaf", "xof", "xpf"];
+
+export default zeroDecimalCurrencies;
